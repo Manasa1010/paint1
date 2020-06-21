f85b36106cab6a4126e9d952f1f29419abb71ba3
Manasa1010 <64597788+Manasa1010@users.noreply.github.com>
2020-06-21 22:18:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canavs;
+var paint;
+var database;
+
+function setup() {
+    database=firebase.database();
+    canvas = createCanvas(800, 800);
+    paint=createSprite(200,200,20,20);
+}
+
+
+function draw() {
+  background(0) ;
+
+
+
+if(mouseIsPressed(paint)){
+    
+}
+
+}
+
+
+
+
